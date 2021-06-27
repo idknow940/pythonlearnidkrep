0480d131678f179cd20313b0127b43fec1df4332
idknow94 <i.dont.kei.94@gmail.com>
2021-06-27 08:56:11 +0400

    new file added

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ def text_func(txt: str, step: int) -> str:
 	txt = txt[::step]
 
 
-print(text_func(a, -1))
+print(text_func(a, 1))
---
+++ upload.bat
@@ -0,0 +1,3 @@
+git add *
+git commit -m "new file added"
+git push
