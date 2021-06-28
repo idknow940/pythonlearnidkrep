3927f4d818b3720e5d3ab6cbc71b00eea1882015
idknow94 <i.dont.kei.94@gmail.com>
2021-06-28 20:28:02 +0400

    changed

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-hello = "Hi!"
-print(hello)
+class Class(object):
+    def __init__(self, name, sur_name):
+        self.name = name
+        self.surname = sur_name
+
+
+obj_1 = Class("Alek", "Aleksanyan")
+
+print(f"{obj_1.name} {obj_1.surname}")
