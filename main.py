7c0090801868972dd3f7f0d81adceaa5354604da
idknow94 <i.dont.kei.94@gmail.com>
2021-06-29 21:19:06 +0400

    Changed main.py

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
-class CV(object):
-    def __init__(self, name, sur_name):
-        self.name = name
-        self.surname = sur_name
+import random
 
-    def present(self):
-        print(f"hello im {self.name} {self.surname}")
+string = "python"
 
 
-obj_1 = CV("Alek", "Aleksanyan")
+class Main:
+    def __init__(self, txt):
+        self.str = txt
+        self.dict = {}
 
-obj_1.present()
+        for i in self.str:
+            self.dict[i] = i
+
+        for i, j in self.dict.items():
+            j = random.randint(0, 3)
+            self.dict[i] = j
+        print(self.dict)
+
+    def rem_dup(self):
+        res = {}
+        for i, j in self.dict.items():
+            if j not in res.values():
+                res[i] = j
+        self.dict = res
+        print(res)
+
+    def print_only_3_highest(self):
+        lst = []
+        for i in self.dict.values():
+            lst.append(i)
+        lst.sort()
+        print(lst[-3:])
+
+
+obj = Main(string)
+
+print(obj.rem_dup())
+print(obj.print_only_3_highest())
