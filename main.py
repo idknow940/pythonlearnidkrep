b461399752b2cfc06ee9923515d1ef84ef4438f5
idknow94 <i.dont.kei.94@gmail.com>
2021-07-01 08:27:18 +0400

    main.py was changed

--- main.py
+++ main.py
@@ -1,38 +1,89 @@
-import random
+import math
+import sys
 
-string = "python"
-
-
-class Main:
-    def __init__(self, txt):
-        self.str = txt
-        self.dict = {}
-
-        for i in self.str:
-            self.dict[i] = i
-
-        for i, j in self.dict.items():
-            j = random.randint(0, 3)
-            self.dict[i] = j
-        print(self.dict)
-
-    def rem_dup(self):
-        res = {}
-        for i, j in self.dict.items():
-            if j not in res.values():
-                res[i] = j
-        self.dict = res
-        print(res)
-
-    def print_only_3_highest(self):
-        lst = []
-        for i in self.dict.values():
-            lst.append(i)
-        lst.sort()
-        print(lst[-3:])
+# Circle Class
 
 
-obj = Main(string)
+class Circle:
+    def __init__(self, r: int) -> None:
+        self.radius = r
+        self.pi = math.pi
+        self.result_area = self.pi * self.radius**2
+        self.result_perimeter = 2 * self.pi * self.radius
 
-print(obj.rem_dup())
-print(obj.print_only_3_highest())
+    def area(self) -> str:
+        """
+        :return: self.result_area
+        """
+
+        return f"the area of the circle is: {self.result_area}"
+
+    def perimeter(self) -> str:
+        """
+        :return self.result_perimeter
+        """
+
+        return f"the perimeter of the circle is: {self.result_perimeter}"
+
+# Triangle Class
+
+
+class Triangle:
+    def __init__(self, h: int, b: int, right_side: int, left_side: int) -> None:
+        self.height = h
+        self.base = b
+        self.right = right_side
+        self.left = left_side
+        self.result_area = b * h / 2
+        self.result_perimeter = self.base + self.left + self.right
+
+    def area(self) -> str:
+        """
+        :arg self
+        :return self.result_area in a string.
+        """
+
+        return f"the area of a triangle is: {self.result_area}"
+
+    def perimeter(self) -> str:
+        """
+        :arg self
+        :return self.result_perimeter in a string.
+        """
+
+        return f"the perimeter of a triangle is: {self.result_perimeter}"
+
+
+radius = input("enter the radius of the circle(int): ")
+
+if radius.isdigit():
+    radius = int(radius)
+else:
+    print("the program needs an integer")
+    sys.exit()
+
+circle = Circle(radius)
+
+print(circle.area())
+print(circle.perimeter())
+
+height = input("enter the height of the triangle(int): ")
+base = input("enter the base of the triangle(int): ")
+right = input("enter the right side of the triangle(int): ")
+left = input("enter the left side of the triangle(int): ")
+
+try:
+    height = int(height)
+    base = int(base)
+    right = int(right)
+    left = int(left)
+
+except ValueError:
+    print("the program needs an integer")
+    sys.exit()
+
+
+triangle = Triangle(height, base, right, left)
+
+print(triangle.area())
+print(triangle.perimeter())
