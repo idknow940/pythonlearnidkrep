55f1c57284bcd50929b76c0ad327ce698bb1de30
idknow94 <i.dont.kei.94@gmail.com>
2021-07-13 20:46:52 +0400

    changed all

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
 import os
-
-for path in os.listdir():
-    if path == "test2":
-        os.chdir(path)
-        for item in os.listdir():
-            if item == "text.txt":
-                os.remove(item)
