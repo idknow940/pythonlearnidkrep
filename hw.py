fe988a9fbcb3ffc86e753a74033325599329ef4e
idknow94 <i.dont.kei.94@gmail.com>
2021-07-19 16:51:21 +0400

    updated hw.py, and some other things lol

--- hw.py
+++ hw.py
@@ -1,58 +1,70 @@
-import os
-import sys
-import json
+# 1
 
-# Variables
-final_price = 0
-final_quantity = 0
-products = []
+# import json
+# import yaml
+#
+# choice = input("yaml or json?: ")
+# if choice == "json":
+#     with open("hw.json", "r") as jj:
+#         data_json = json.load(jj)
+#
+#     with open("hw.txt", "w") as jt:
+#         json.dump(data_json, jt, indent=4)
+#
+#     with open("main.yaml", "w") as jy:
+#         yaml.dump(data_json, jy)
+# elif choice == "yaml":
+#     with open("main.yaml", "r") as yy:
+#         data_yaml = yaml.load(yy)
+#
+#     with open("hw.json", "w") as yj:
+#         json.dump(data_yaml, yj, indent=4)
+#
+#     with open("hw.txt", "w") as yt:
+#         yaml.dump(data_yaml, yt, indent=4)
+# else:
+#     print(":/")
 
-dct = {}
+# 2
 
-# Main Loop
-while True:
-    # Input
-    data = input("name, price, quantity(separate with ', '): ").split(",")
-
-    # Main Info
-    dict_product = dict(
-        name=data[0],
-        price=data[1],
-        quantity=data[2]
-    )
-
-    # Final Variables
-    final_price += float(data[1])
-    final_quantity += float(data[2])
-
-    # Final Dict
-    dict_end = dict(
-        final_price=final_price,
-        final_quantity=final_quantity
-    )
-
-    products.append(dict_product)
-
-    # Leave Logic
-
-    # Input
-    end = input("leave the loop(y/n): ")
-
-    # Statements
-    if end == "y":
-        products.append(dict_end)
-        dct["items"] = products
-        break
-    elif end == "n":
-        pass
-    else:
-        print("????")
-        sys.exit()
-
-# Dumping Products List In product.json
-with open("hw.json", "w") as pj:
-    json.dump(dct, pj, indent=4)
-    print("Written your product to the .json file!")
-
-with open("hw.json", "r") as pj:
-    print(f"The dictionary you wrote: {json.load(pj)}")
+# import requests
+#
+# with open("hw.txt", "r") as file:
+#     txt = file.read()
+#
+#
+# class Methods:
+#     def __init__(self, text):
+#         self.links = text.split("\n")
+#
+#     def download_links(self):
+#         for i in self.links:
+#             response = requests.get(i)
+#             num = 0
+#             with open(f"img{num}.png", "wb") as F:
+#                 F.write(response.content)
+#             num += 1
+#
+#     def download_links_jpeg(self):
+#         num = 0
+#         for i in self.links:
+#             if "jpg" in i or "JPG" in i:
+#                 response = requests.get(i)
+#                 with open(f"img{num}.jpg", "wb") as f:
+#                     f.write(response.content)
+#                 num += 1
+#
+#     def download_links_png(self):
+#         num = 0
+#         for i in self.links:
+#             if "png" in i or "PNG" in i:
+#                 response = requests.get(i)
+#                 with open(f"img{num}.png", "wb") as f:
+#                     f.write(response.content)
+#                 num += 1
+#
+#
+# py_imgs = Methods(txt)
+#
+# py_imgs.download_links_jpeg()
+# py_imgs.download_links_png()
